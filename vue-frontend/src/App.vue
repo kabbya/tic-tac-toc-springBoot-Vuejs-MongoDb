a9efe6a793f568cdb831d3910ff58398281bda59
kabbya <kabbya.kantam@gmail.com>
2022-03-20 20:08:17 +0600

    Added bootstrap

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Game/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Game from "./components/Game.vue";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Game
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+ -vebkit-font-snoothing: antiatiased;
+ -noz-osx-Tont-snoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+ /* margin-top: 60px; */
 }
 </style>
